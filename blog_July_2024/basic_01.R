a4dd7bafaa09385150fbaca02ae96c8058f25d50
jimrothstein <jimrothstein@gmail.com>
2025-03-05 20:58:00 -0800

    1st step in following blog

---
+++ blog_July_2024/basic_01.R
@@ -0,0 +1,36 @@
+## https://pharmaverse.github.io/blog/posts/2024-07-22_teal_app_development_pharmaverseadam/teal-app-development.html
+
+pak::pak(c("teal", "pharmaverseadam"))
+
+# Throws ERROR, but not in reprex!
+reprex::reprex({
+# Step 1: import packages
+  
+library(teal)
+library(pharmaverseadam)
+
+data = teal.data::teal_data(
+  ADAE = pharmaverseadam::adae,
+  ADSL = pharmaverseadam::adsl 
+)
+data = teal.data::teal_data(
+  IRIS = iris
+)
+
+
+
+# Step 2: create a teal data object
+data <- cdisc_data(
+  ADAE = pharmaverseadam::adae,
+  ADSL = pharmaverseadam::adsl
+)
+})
+
+# Step 3: initialize teal app
+app <- init(
+  data = data,
+  modules = example_module()
+)
+
+# Step 4: run shiny app
+shinyApp(app$ui, app$server)
