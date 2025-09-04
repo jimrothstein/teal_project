a7b31378d7fb643692347293eff19664fc575ad9
jimrothstein <jimrothstein@gmail.com>
2025-09-03 22:45:10 -0700

    wip

--- blog_July_2024/basic_01.R
+++ blog_July_2024/basic_01.R
@@ -1,9 +1,12 @@
+## REF:
 ## https://pharmaverse.github.io/blog/posts/2024-07-22_teal_app_development_pharmaverseadam/teal-app-development.html
+
+## PURPOSE:   construct simple TEAL app using ADaM data
+## WORKS
 
 pak::pak(c("teal", "pharmaverseadam"))
 
 # Throws ERROR, but not in reprex!
-reprex::reprex({
 # Step 1: import packages
   
 library(teal)
@@ -13,6 +16,8 @@ data = teal.data::teal_data(
   ADAE = pharmaverseadam::adae,
   ADSL = pharmaverseadam::adsl 
 )
+
+# use iris data instead
 data = teal.data::teal_data(
   IRIS = iris
 )
@@ -24,7 +29,6 @@ data <- cdisc_data(
   ADAE = pharmaverseadam::adae,
   ADSL = pharmaverseadam::adsl
 )
-})
 
 # Step 3: initialize teal app
 app <- init(
